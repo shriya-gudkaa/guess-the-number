a3b192ab58a9ad6364651becfbfc94e84618a098
shriya gudka <gudkashriya@gmail.com>
2025-08-19 22:26:21 +0530

    Update guess the number.py

--- guess the number.py
+++ guess the number.py
@@ -1,6 +1,8 @@
 import random
 import time
 
+high_score = 0  
+
 while True:
     num_guess = random.randint(1, 20)
     guess = None
@@ -30,15 +32,21 @@ while True:
     total_time = end_time - start_time
 
     if guess == num_guess:
-        
         score = max_score - (tries - 1) * 10 - int(total_time)
         if score < 0:
             score = 0
         print(f"Your score: {score}")
+
+        if score > high_score:
+            high_score = score
+            print("Congratulations! You set a new high score!")
+        else:
+            print(f"The current high score is {high_score}")
     else:
         print("Better luck next time!")
         print(f"The correct number was: {num_guess}")
         print("Your score: 0")
+        print(f"The current high score is {high_score}")
 
     print(f"You took {total_time:.2f} seconds to guess the number!")
     play_again = input("Do you want to play again? (y/n): ").lower()
